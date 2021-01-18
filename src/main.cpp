231f58104c231d6932ff7d25ec5a65b4587a864e
artemyv <artemyv@gmail.com>
2021-01-18 22:08:31 +0200

    sample stream demo wrapper

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <spdlog/sinks/stdout_color_sinks.h> // or "../stdout_sinks.h" if no colors needed
 #include <spdlog/spdlog.h>
+#include "logger.h"
 
 int main() {
     SPDLOG_INFO("Hello from spdlog!");
 
+    LOG(ERROR) << "AAAA" << 123;
     return 0;
 }
