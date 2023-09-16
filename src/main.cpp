de47fc55e1dfaf27459a528b84368446857d90fc
artemyv <artemyv@gmail.com>
2023-09-16 21:58:34 +0300

    Add project version and description to output https://cmake.org/cmake/help/latest/guide/tutorial/A%20Basic%20Starting%20Point.html

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <spdlog/sinks/stdout_color_sinks.h> // or "../stdout_sinks.h" if no colors needed
 #include <spdlog/spdlog.h>
 
+#include <version.h>
+
 int main() {
-    SPDLOG_INFO("Hello from spdlog!");
+    SPDLOG_INFO("Hello from project: {}.", cmake_spdlog_example_DESCRIPTION);
+    SPDLOG_INFO("Version: {}", cmake_spdlog_example_VERSION);
+	
     return 0;
 }
