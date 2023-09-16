1a329565de573611c26c9b748dc0efc980009d42
artemyv <artemyv@gmail.com>
2023-09-16 11:05:57 +0300

    Specify build tools location

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 mkdir build
-cmake -G "Visual Studio 17 2022" -H. -Bbuild
+cmake -G "Visual Studio 17 2022" -H. -Bbuild -DCMAKE_GENERATOR_INSTANCE="D:\msbuild_test",version=17.7.34031.279
 cmake --build build
