4d609d858968c245dba20f78133119e85e34515b
artemyv <artemyv@gmail.com>
2023-09-16 11:19:28 +0300

    Select VS2019 tollset (v142) to be used with VS2022 msbuild

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 mkdir build
-cmake -G "Visual Studio 17 2022" -H. -Bbuild -DCMAKE_GENERATOR_INSTANCE="D:\msbuild_test",version=17.7.34031.279
+cmake -G "Visual Studio 17 2022" -H. -Bbuild -DCMAKE_GENERATOR_INSTANCE="D:\msbuild_test",version=17.7.34031.279 -DCMAKE_GENERATOR_TOOLSET=v142
 cmake --build build
