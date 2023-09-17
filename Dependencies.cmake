485ac6456ba88794a7a566bb566f626a46811d3b
artemyv <artemyv@gmail.com>
2023-09-17 08:38:54 +0300

    Change dependency manager to CPM

---
+++ Dependencies.cmake
@@ -0,0 +1,31 @@
+include(cmake/CPM.cmake)
+
+# Done as a function so that updates to variables like
+# CMAKE_CXX_FLAGS don't propagate out to other
+# targets
+function(myproject_setup_dependencies)
+
+  # For each dependency, see if it's
+  # already been provided to us by a parent project
+
+  if(NOT TARGET fmtlib::fmtlib)
+    cpmaddpackage("gh:fmtlib/fmt#10.1.1")
+  endif()
+
+  if(NOT TARGET spdlog::spdlog)
+    cpmaddpackage(
+      NAME
+      spdlog
+      VERSION
+      1.12.0
+      GITHUB_REPOSITORY
+      "gabime/spdlog"
+      OPTIONS
+      "SPDLOG_FMT_EXTERNAL ON")
+  endif()
+
+  if(NOT TARGET Catch2::Catch2WithMain)
+    cpmaddpackage("gh:catchorg/Catch2@3.4.0")
+  endif()
+
+endfunction()
