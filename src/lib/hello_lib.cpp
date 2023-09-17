9bc2f719b058702c251bf9a593baf2d875820cd3
artemyv <artemyv@gmail.com>
2023-09-17 08:50:59 +0300

    add lib

---
+++ src/lib/hello_lib.cpp
@@ -0,0 +1,14 @@
+﻿#include "hello_lib.h"
+
+#include <version.h>
+#include <fmt/format.h>
+
+std::string hello_lib::getDescription()
+{
+	return fmt::format("Hello from project: {}.", cmake_spdlog_example_DESCRIPTION);
+}
+ 
+std::string hello_lib::getVersion()
+{
+	return fmt::format("Version: {}", cmake_spdlog_example_VERSION);
+}
---
+++ src/lib/hello_lib.h
@@ -0,0 +1,8 @@
+﻿#include <string>
+
+namespace hello_lib
+{
+std::string getDescription();
+ 
+std::string getVersion();
+}
