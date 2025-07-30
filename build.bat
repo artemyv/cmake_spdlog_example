bf3b1d5f8fb2eb4db9d7f7a71eebc403497c715e
artemyv <artemyv@gmail.com>
2025-07-30 19:29:12 +0300

    skse based macros

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
 mkdir build
-cmake -G "Visual Studio 17 2022" -H. -Bbuild -DCMAKE_GENERATOR_INSTANCE="D:\msbuild_test",version=17.7.34031.279 -DCMAKE_GENERATOR_TOOLSET=v142
-cmake --build build
+cmake -H. -Bbuild 
+cmake --build build --config Release --verbose
+cmake --install build --prefix Release --config Release
+ctest -G
+ctest -VV -C Release
--- src/exe/main.cpp
+++ src/exe/main.cpp
@@ -1,10 +1,48 @@
 #include <spdlog/sinks/stdout_color_sinks.h> // or "../stdout_sinks.h" if no colors needed
 #include <spdlog/spdlog.h>
+#include  <source_location>
+#include <string>
+#define SKSE_MAKE_SOURCE_LOGGER(a_func, a_type)                                       \
+                                                                                      \
+	template <class... Args>                                                          \
+	struct [[maybe_unused]] a_func                                                    \
+	{                                                                                 \
+		a_func() = delete;                                                            \
+                                                                                      \
+		explicit a_func(                                                              \
+			fmt::format_string<Args...> a_fmt,                                        \
+			Args&&... a_args,                                                         \
+			std::source_location a_loc = std::source_location::current()) \
+		{                                                                             \
+			spdlog::log(                                                              \
+				spdlog::source_loc{                                                   \
+					a_loc.file_name(),                                                \
+					static_cast<int>(a_loc.line()),                                   \
+					a_loc.function_name() },                                          \
+				spdlog::level::a_type,                                                \
+				a_fmt,                                                                \
+				std::forward<Args>(a_args)...);                                       \
+		}                                                                             \
+	};                                                                                \
+                                                                                      \
+	template <class... Args>                                                          \
+	a_func(fmt::format_string<Args...>, Args&&...) -> a_func<Args...>;
 
-#include <hello_lib.h>
+namespace Log
+{
+	SKSE_MAKE_SOURCE_LOGGER(trace, trace);
+	SKSE_MAKE_SOURCE_LOGGER(debug, debug);
+	SKSE_MAKE_SOURCE_LOGGER(info, info);
+	SKSE_MAKE_SOURCE_LOGGER(warn, warn);
+	SKSE_MAKE_SOURCE_LOGGER(err, err);
+	SKSE_MAKE_SOURCE_LOGGER(critical, critical);
+}
 
-int main() {
-    SPDLOG_INFO(_TXT("{}."), hello_lib::getDescription());
-    SPDLOG_INFO(_TXT("{}."), hello_lib::getVersion());
+int main()
+{
+    Log::info("Hello info from {}.", "main.cpp");
+    Log::err("Hello err from {}.", "main.cpp");
+    Log::warn("Hello warn from {}.", "main.cpp");
+    Log::debug("Hello debug from {}.", "main.cpp");
     return 0;
 }
